afad5df486ddf0911ca2e8dc1b31a7ef76d26959
UPye <75226236+UPye@users.noreply.github.com>
2021-02-23 23:24:23 -0500

    Set up the project, setup the testing framework, wrote a test that failed the Potion Creation, wrote a test that passed the Potion Creation, and refactored the code

---
+++ .history/_tests_/Potion.test_20210223224108.js
@@ -0,0 +1,9 @@
+const { TestScheduler } = require('jest');
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
---
+++ .history/_tests_/Potion.test_20210223224148.js
@@ -0,0 +1,9 @@
+const { TestScheduler } = require('jest');
+const Potion = require('../lib/Potion.js');
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual();
+    expect(potion.value).toEqual(expect.any(Number));
+});
